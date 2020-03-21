1df64647116e399d1b1dabb67927ac9e10f50088
Sylvan <sylvan.j.morris@gmail.com>
2020-03-21 15:08:28 +0200

    Adding snake

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,67 @@
-use piston_window::*;
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
 
-fn main() {
-    let mut window: PistonWindow = 
-        WindowSettings::new("Hello Piston!", [640, 480])
-            .exit_on_esc(true).build().unwrap();
-    
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, | context, graphics, _device | {
-            clear([1.0; 4], graphics);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                [0.0, 0.0, 100.0, 100.0],
-                context.transform,
-                graphics);
-        });
+use piston::window::WindowSettings;
+use piston::event_loop:: *;
+use piston::input::*;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+
+
+struct Game {
+    gl: GlGraphics,
+    snake: Snake,
+}
+
+impl Game {
+    fn render(&mut self, arg: &RenderArgs) {
+    use graphics;
+
+
+    let GREY: [f32; 4] = [0.788, 0.808, 0.741, 1.0];
+    let LIGHT_GREEN: [f32; 4] = [0.698, 0.737, 0.667, 1.0];
+    let GREEN: [f32; 4] = [0.514, 0.557, 0.514, 1.0];
+    let BROWN: [f32; 4] = [0.424, 0.376, 0.380, 1.0];
+    let RED: [f32; 4] = [0.392, 0.251, 0.243, 1.0];
+
+    self.gl.draw(arg.viewport(), |_c, gl| {
+        graphics::clear(GREEN, gl);
+    });
+
     }
+}
+
+struct Snake {
+    pos_x: i32,
+    pos_y: i32,
+}
+
+impl Snake {
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs)
+}
+fn main() {
+    let opengl = OpenGL::V4_5;
+
+    let mut window: GlutinWindow = WindowSettings::new(
+        "Vipers",
+         [200, 200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+        let mut game = Game {
+            gl: GlGraphics::new(opengl)
+        };
+
+        let mut events = Events::new(EventSettings::new());
+        while let Some(e) = events.next(&mut window) {
+
+            if let Some(r) = e.render_args() {
+                game.render(&r);
+            }
+        }
+
 }
