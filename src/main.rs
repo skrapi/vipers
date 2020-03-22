59213797268535e85cdc0ffe787bf1b683b3ee51
Sylvan <sylvan.j.morris@gmail.com>
2020-03-22 13:17:47 +0200

    Made the food change position when the snake's head is in the same block

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
 extern crate opengl_graphics;
+extern crate rand;
 
 use piston::window::WindowSettings;
 use piston::event_loop:: *;
@@ -12,6 +13,8 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use std::collections::LinkedList;
 use std::iter::FromIterator;
 
+use rand::Rng;
+
 #[derive(Clone, PartialEq)]
 enum Direction {
     Right,
@@ -20,8 +23,14 @@ enum Direction {
     Down
 }
 
+struct Size {
+    width: u32,
+    height: u32,
+}
+
 struct Game {
     gl: GlGraphics,
+    size: Size,
     snake: Snake,
     food: Food,
 }
@@ -45,6 +54,11 @@ impl Game {
 
     fn update(&mut self) {
         self.snake.update();
+        
+        let snake_head = (*self.snake.body.front().expect("Snake has no body")).clone();
+        if (snake_head.0 == self.food.x) && (snake_head.1 == self.food.y) {
+            self.food.update();
+        }
     }
 
     fn pressed(&mut self, btn: &Button) {
@@ -65,32 +79,30 @@ impl Game {
 }
 
 struct Snake {
-    body: LinkedList<(i32, i32)>,
+    body: LinkedList<(u32, u32)>,
     dir: Direction,
 }
 
 impl Snake {
     fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        let red: [f32; 4] = [0.392, 0.251, 0.243, 1.0];
 
-    let _brown: [f32; 4] = [0.424, 0.376, 0.380, 1.0];
-    let red: [f32; 4] = [0.392, 0.251, 0.243, 1.0];
-
-    let squares: Vec<graphics::types::Rectangle> = self.body
-        .iter()
-        .map(|&(x,y)| {
-            graphics::rectangle::square(
-                (x * 20) as f64, 
-                (y * 20) as f64, 
-                20_f64)
-        })
-        .collect();
+        let squares: Vec<graphics::types::Rectangle> = self.body
+            .iter()
+            .map(|&(x,y)| {
+                graphics::rectangle::square(
+                    (x * 20) as f64, 
+                    (y * 20) as f64, 
+                    20_f64)
+            })
+            .collect();
 
 
-    gl.draw(args.viewport(), |c, gl| {
-        let transform = c.transform;
-        squares.into_iter()
-            .for_each(|square| graphics::rectangle(red, square, transform, gl)); 
-    });
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+            squares.into_iter()
+                .for_each(|square| graphics::rectangle(red, square, transform, gl)); 
+        });
     }
 
     fn update (&mut self) {
@@ -110,13 +122,12 @@ impl Snake {
 }
 
 struct Food {
-    x: i32,
-    y: i32,
+    x: u32,
+    y: u32,
 }
 
 impl Food {
     fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
-
         let brown: [f32; 4] = [0.424, 0.376, 0.380, 1.0];
     
         let square = graphics::rectangle::square(
@@ -129,53 +140,68 @@ impl Food {
             
             graphics::rectangle(brown, square, transform, gl); 
         });
-        }
+    }
+
+    fn update(&mut self) {
+        // TODO make this not land on snake
+        self.x = rand::random::<u8>() as u32;
+        self.y = rand::random::<u8>() as u32;
+    }
 }
 fn main() {
     let opengl = OpenGL::V4_5;
 
+    let mut rng = rand::thread_rng();
+
+    let width = 640;
+    let height = 480;
+
     let mut window: GlutinWindow = WindowSettings::new(
         "Vipers",
-         [640, 480])
+         [width, height])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-        let mut game = Game {
-            gl: GlGraphics::new(opengl),
-            snake: Snake { 
-                body: LinkedList::from_iter((vec![(0,0), (0,1)]).into_iter()), 
-                dir: Direction::Right 
-            },
-            food: Food {
-                x: 5,
-                y: 5,
-            }
-        };
+    let mut game = Game {
+        gl: GlGraphics::new(opengl),
+        size: Size {
+            width,
+            height,
+        },
+        snake: Snake { 
+            body: LinkedList::from_iter((vec![(0,0), (0,1)]).into_iter()), 
+            dir: Direction::Right 
+        },
+        food: Food {
+            x: rng.gen_range(0, width/20),
+            y: rng.gen_range(0, height/20),
+        }
+    };
 
-        let mut events = Events::new(EventSettings::new()).ups(8);
+    let mut events = Events::new(EventSettings::new()).ups(8);
 
-        while let Some(event) = events.next(&mut window) {
+    while let Some(event) = events.next(&mut window) {
 
-            if let Some(r) = event.render_args() {
-                game.render(&r);
-            }
+        if let Some(r) = event.render_args() {
+            game.render(&r);
+        }
 
-            if let Some(_) = event.update_args() {
-                game.update();
-            }
+        if let Some(_) = event.update_args() {
+            game.update();
+        }
 
-            if let Some(k) = event.button_args() {
-                if k.state == ButtonState::Press {
-                    if k.button == Button::Keyboard(Key::Escape){
-                        break;
-                    }
-                    else {
-                        game.pressed(&k.button);
-                    }
+        if let Some(k) = event.button_args() {
+            if k.state == ButtonState::Press {
+                if k.button == Button::Keyboard(Key::Escape){
+                    break;
+                }
+                else {
+                    game.pressed(&k.button);
                 }
             }
         }
+    }
 
 }
